f978c6237161a130058f0c0b962eaa8773d89ace
Jose Manuel Samano Miijangos <josemsamano@hotmail.com>
2015-02-26 16:11:34 -0600

    Initial commit

---
+++ ParseStarterProject/src/com/parse/starter/ParseApplication.java
@@ -0,0 +1,32 @@
+package com.parse.starter;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseCrashReporting;
+import com.parse.ParseUser;
+
+public class ParseApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Initialize Crash Reporting.
+    ParseCrashReporting.enable(this);
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+    Parse.initialize(this, "NUozCFi35BS24T93XpaXsEkIzEH9nUXz8a0eXTB8", "tChDAHWKDp1IsZkRzoc8Q6Cb1SLX0YOe91BNufot");
+
+
+    ParseUser.enableAutomaticUser();
+    ParseACL defaultACL = new ParseACL();
+    // Optionally enable public read access.
+    // defaultACL.setPublicReadAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+  }
+}
---
+++ ParseStarterProject/src/com/parse/starter/SingleItemView.java
@@ -0,0 +1,37 @@
+package com.parse.starter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by Jose Manuel on 26/02/2015.
+ */
+
+public class SingleItemView extends Activity {
+    // Declare Variables
+    TextView txtname;
+    String name;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get the view from singleitemview.xml
+        setContentView(R.layout.singleitemview);
+
+        // Retrieve data from MainActivity on item click event
+        Intent i = getIntent();
+
+        // Get the name
+        name = i.getStringExtra("name");
+
+        // Locate the TextView in singleitemview.xml
+        txtname = (TextView) findViewById(R.id.name);
+
+        // Load the text into the TextView
+        txtname.setText(name);
+
+    }
+
+}
